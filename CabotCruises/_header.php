0a44be9312dec54aba6e567d0b0c317bbb27e29d
ayodyln <78442693+ayodyln@users.noreply.github.com>
2023-02-09 21:58:39 -0700

    update to php

---
+++ CabotCruises/_header.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- <link rel="stylesheet" href="/style.css" /> -->
+    <!-- <link rel="stylesheet" type="text/css" href="slick-1.8.1/slick/slick.css" /> -->
+    <!-- <link rel="stylesheet" type="text/css" href="slick-1.8.1/slick/slick-theme.css" /> -->
+
+
+    <title>Cabot Cruises</title>
+</head>
+
+
+<body>
+    <style>
+        <?php include('style.css') ?><?php include('slick-1.8.1/slick/slick.css') ?><?php include('slick-1.8.1/slick/slick-theme.css') ?>
+    </style>
+
+
+    <section id="header__wrapper">
+        <header>
+            <div id="logo">
+                <img src="./content/logo_assets/logo.png" alt="Cabot Cruises Logo" srcset="
+              ./content/logo_assets/logo1.png 1x,
+              ./content/logo_assets/logo2.png 2x,
+              ./content/logo_assets/logo3.png 3x
+            " />
+            </div>
+
+            <a href="tel:+18005551234">1-800-555-1234</a>
+        </header>
+    </section>
+
+    <section id="nav__wrapper">
+        <div id="nav">
+            <nav>
+                <button id="hamburgerBtn">&#9776;</button>
+                <ul id="primaryNav">
+                    <li><a href="#">Home</a></li>
+                    <li class="parent">
+                        <a href="#">Cruises</a>
+                    </li>
+                    <li class="parent">
+                        <a href="#">Agents</a>
+                        <ul>
+                            <li><a href="#">Sally Rodgers</a></li>
+                            <li><a href="#">Buddy Hall</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Book</a></li>
+                </ul>
+            </nav>
+
+            <form id="searchInput">
+                <input type="search" name="search" id="search" placeholder="Search..." />
+
+                <button value="search">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                        <!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+                        <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352c79.5 0 144-64.5 144-144s-64.5-144-144-144S64 128.5 64 208s64.5 144 144 144z" />
+                    </svg>
+                </button>
+            </form>
+        </div>
+    </section>
+
+    <main>
---
+++ CabotCruises/index.php
@@ -0,0 +1,66 @@
+<?php include('_header.php') ?>
+
+<section id="main__wrapper">
+  <div class="Slider">
+    <picture>
+      <source media="(max-width: 350px)" srcset="./content/images/slider1-square.jpg" />
+      <source media="(max-width: 390px)" srcset="./content/images/slider1-390px.jpg" />
+      <source media="(max-width: 560px)" srcset="./content/images/slider1-560px.jpg" />
+      <source media="(max-width: 1028px)" srcset="./content/images/slider1-1028px.jpg" />
+      <img src="./content/images/slider1.jpg" alt="Cruise Image One" />
+    </picture>
+
+    <picture>
+      <source media="(max-width: 350px)" srcset="./content/images/slider2-square.jpg" />
+      <source media="(max-width: 390px)" srcset="./content/images/slider2-390px.jpg" />
+      <source media="(max-width: 560px)" srcset="./content/images/slider2-560px.jpg" />
+      <source media="(max-width: 1028px)" srcset="./content/images/slider2-1028px.jpg" />
+      <img src="./content/images/slider2.jpg" alt="Cruise Image One" />
+    </picture>
+
+    <picture>
+      <source media="(max-width: 350px)" srcset="./content/images/slider3-square.jpg" />
+      <source media="(max-width: 390px)" srcset="./content/images/slider3-390px.jpg" />
+      <source media="(max-width: 560px)" srcset="./content/images/slider3-560px.jpg" />
+      <source media="(max-width: 1028px)" srcset="./content/images/slider3-1028px.jpg" />
+      <img src="./content/images/slider3.jpg" alt="Cruise Image One" />
+    </picture>
+
+    <picture>
+      <source media="(max-width: 350px)" srcset="./content/images/slider4-square.jpg" />
+      <source media="(max-width: 390px)" srcset="./content/images/slider4-390px.jpg" />
+      <source media="(max-width: 560px)" srcset="./content/images/slider4-560px.jpg" />
+      <source media="(max-width: 1028px)" srcset="./content/images/slider4-1028px.jpg" />
+      <img src="./content/images/slider4.jpg" alt="Cruise Image One" />
+    </picture>
+  </div>
+
+  <section id="BlogArticles">
+    <h2>Recent Blog Articles</h2>
+
+    <div>
+      <a href="#" class="blogCTA">
+        <span>Caribbean Cruises</span>
+        <img src="./content/articles/blog_1-square.jpg" alt="Article 1" />
+        <span>Date Info</span>
+      </a>
+      <a href="#" class="blogCTA">
+        <span>Blog Title</span>
+        <img src="./content/articles/blog_2-square.jpg" alt="Article 2" />
+        <span>Date Info</span>
+      </a>
+      <a href="#" class="blogCTA">
+        <span>Blog Title</span>
+        <img src="./content/articles/blog_3-article.jpg" alt="Article 3" />
+        <span>Date Info</span>
+      </a>
+      <a href="#" class="blogCTA">
+        <span>Blog Title</span>
+        <img src="./content/articles/blog_4-article.jpg" alt="Article 4" />
+        <span>Date Info</span>
+      </a>
+    </div>
+  </section>
+</section>
+
+<?php include('_footer.php') ?>
