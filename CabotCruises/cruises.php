eb5bc24f15520f4ac8b59314344f19486bdfcb86
ayodyln <78442693+ayodyln@users.noreply.github.com>
2023-02-10 18:53:11 -0700

    Cruise page half way done

--- CabotCruises/cruises.php
+++ CabotCruises/cruises.php
@@ -1,7 +1,7 @@
 <?php
 $title = "Cruises";
 include('_header.php')
-?>
+    ?>
 
 <section id="CruisesPage">
     <h1>
@@ -11,40 +11,51 @@ include('_header.php')
         <div class="pkg-1">
             <img src="./content/originals/blog_1.jpg" alt="Cruise Package 1">
             <section>
-                <h3></h3>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis vel perspiciatis optio inventore
-                    eum, laborum quasi doloribus ducimus ipsum ab fugit numquam iste distinctio tempore molestiae
-                    exercitationem reiciendis assumenda eveniet!</p>
+                <h3>Australia & New Zealand Cruises from $999!</h3>
+                <p>
+                    On the other side of the world, Australia and New Zealand are full of wonders to behold! Now, more
+                    than ever before, you can get phenomenal discounts to cruise the South Pacific. Sail to New Zealand
+                    from Sydney, Australia on a 12-day cruise from just $888 per person plus taxes.
+                </p>
                 <button>Read More</button>
             </section>
         </div>
         <div class="pkg-2">
-            <img src="./content/originals/blog_1.jpg" alt="">
+            <img src="./content/originals/blog_1.jpg" alt="Cruise Package 2">
             <section>
-                <h3></h3>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis vel perspiciatis optio inventore
-                    eum, laborum quasi doloribus ducimus ipsum ab fugit numquam iste distinctio tempore molestiae
-                    exercitationem reiciendis assumenda eveniet!</p>
+                <h3>Title of Cruize with $price!</h3>
+                <p>
+                    Grab some real text for these descrip5ons and pase it in here. Cruise the Pacific Coast including
+                    Canada and San Francisco in Fall 2018. Larry Gelwix the “Getaway Guru®” will be your cruise host
+                    while you visit Astoria, Oregon and Victoria, B.C. Plus, spend 2 days visi5ng San Francisco! The
+                    highlight of this cruise will be sailing into and out .
+                </p>
                 <button>Read More</button>
             </section>
         </div>
         <div class="pkg-3">
-            <img src="./content/originals/blog_1.jpg" alt="">
+            <img src="./content/originals/blog_1.jpg" alt="Cruise Package 3">
             <section>
-                <h3></h3>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis vel perspiciatis optio inventore
-                    eum, laborum quasi doloribus ducimus ipsum ab fugit numquam iste distinctio tempore molestiae
-                    exercitationem reiciendis assumenda eveniet!</p>
+                <h3>Title of Cruize with $price!</h3>
+                <p>
+                    Grab some real text for these descrip5ons and pase it in here. Cruise the Pacific Coast including
+                    Canada and San Francisco in Fall 2018. Larry Gelwix the “Getaway Guru®” will be your cruise host
+                    while you visit Astoria, Oregon and Victoria, B.C. Plus, spend 2 days visi5ng San Francisco! The
+                    highlight of this cruise will be sailing into and out .
+                </p>
                 <button>Read More</button>
             </section>
         </div>
         <div class="pkg-4">
-            <img src="./content/originals/blog_1.jpg" alt="">
+            <img src="./content/originals/blog_1.jpg" alt="Cruise Package 4">
             <section>
-                <h3></h3>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis vel perspiciatis optio inventore
-                    eum, laborum quasi doloribus ducimus ipsum ab fugit numquam iste distinctio tempore molestiae
-                    exercitationem reiciendis assumenda eveniet!</p>
+                <h3>Title of Cruize with $price!</h3>
+                <p>
+                    Grab some real text for these descrip5ons and pase it in here. Cruise the Pacific Coast including
+                    Canada and San Francisco in Fall 2018. Larry Gelwix the “Getaway Guru®” will be your cruise host
+                    while you visit Astoria, Oregon and Victoria, B.C. Plus, spend 2 days visi5ng San Francisco! The
+                    highlight of this cruise will be sailing into and out .
+                </p>
                 <button>Read More</button>
             </section>
         </div>
