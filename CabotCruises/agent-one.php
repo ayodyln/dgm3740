268f9f71a34eea6ecd5a9034519eabf29de2fc8c
ayodyln <78442693+ayodyln@users.noreply.github.com>
2023-02-11 16:43:45 -0700

    Agent pages created

--- CabotCruises/agent-one.php
+++ CabotCruises/agent-one.php
@@ -11,47 +11,64 @@ include('_top.php')
         <img src="./content/originals/AgentOne.jpg" alt="Adam Smith">
         <div>
             <h3>Phone:</h3>
-            <a href="tel:+1-800-555-1234">1-800-555-1234</a>
+            <a href="tel:+1-800-123-1234">1-800-123-1234</a>
         </div>
         <div>
             <h3>Working:</h3>
-            <p>Monday, Wednesday, Friday</p>
+            <p>Monday, Thursday, Friday</p>
         </div>
         <div>
             <h3>Destination Specialization:</h3>
             <p>
-                Israel, Egypt, Italy, Greece, Hawaii, Guatemala, Southern California, Orlando, Las Vegas, Bahamas,
-                Caribbean cruises.
+                Caribbean, Bahamas, Bermuda, Alaska, and Hawaii Cruises.
             </p>
         </div>
         <div>
             <h3>Office Hours:</h3>
-            <p>8:00am-6:00pm</p>
+            <p>8:00am-4:00pm</p>
         </div>
         <div>
             <h3>Biography:</h3>
             <p>
-                I have loved travel for as long as I can remember. At the age of 12 I asked if I could fly by myself to
-                Hawaii and I never came back.
-                Right out of high school I started into travel school and have had the opportunity to work in many
-                aspects of the industry. In every aspect I have worked I have had the opportunity to help people travel,
-                to help fulfill people?s dreams, what could be be6er to do every day!
+                I'm an advid traveller myself! Every destination I offer services for have been my favorite places to
+                travel. At a young age, I always wanted to travel the world. After college, I took that goal and made it
+                a reality, then built a career around it.
+            </p>
+            <p>
+                If you have a place you want to go, I can figure out how to get you there. My passion is travel to
+                places that inspire my heart, so I understand that want to go places. It is my goal to get you where you
+                want in this world and make the best memories ever!
             </p>
         </div>
         <div>
             <h3>Customer Comments:</h3>
 
             <p>
-                "Thank you Wendy for all your planning and efforts to ensure that we could enjoy our tour and bring back
-                such wonderful memories." -John Doe
+                "This was the best trip ever and the kids enjoyed every moment of it. Thank you so much for getting us
+                to such a magical place for a great price!"
+
+                <br>
+                <br>
+
+                -Williams Family
             </p>
             <p>
-                "Thank you Wendy for all your planning and efforts to ensure that we could enjoy our tour and bring back
-                such wonderful memories." -John Doe
+                "I always wanted to travel the world and never knew where to start. Thanks to Adam, I was to find the
+                best way to travel and a pretty good deal."
+
+                <br>
+                <br>
+
+                -Blake
             </p>
             <p>
-                "Thank you Wendy for all your planning and efforts to ensure that we could enjoy our tour and bring back
-                such wonderful memories." -John Doe
+                "I was able to see the best views of Hawaii for a super cheap price for my wife and a I. We were working
+                on a budget and always wanted to travel somewhere! Thank you!"
+
+                <br>
+                <br>
+
+                -Roger
             </p>
         </div>
     </div>
--- CabotCruises/agent-two.php
+++ CabotCruises/agent-two.php
@@ -15,43 +15,64 @@ include('_top.php')
         </div>
         <div>
             <h3>Working:</h3>
-            <p>Monday, Wednesday, Friday</p>
+            <p>Monday, Tuesday, Thursday</p>
         </div>
         <div>
             <h3>Destination Specialization:</h3>
             <p>
-                Israel, Egypt, Italy, Greece, Hawaii, Guatemala, Southern California, Orlando, Las Vegas, Bahamas,
-                Caribbean cruises.
+                Europe, Mediterranean, Greece, and Italy Cruises!
             </p>
         </div>
         <div>
             <h3>Office Hours:</h3>
-            <p>8:00am-6:00pm</p>
+            <p>9:00am-4:00pm</p>
         </div>
         <div>
             <h3>Biography:</h3>
             <p>
-                I have loved travel for as long as I can remember. At the age of 12 I asked if I could fly by myself to
-                Hawaii and I never came back.
-                Right out of high school I started into travel school and have had the opportunity to work in many
-                aspects of the industry. In every aspect I have worked I have had the opportunity to help people travel,
-                to help fulfill people?s dreams, what could be be6er to do every day!
+                My husband and I have been traveling ever since we first met. Both of us had goals to travel the world,
+                but fell for each other as well. So, we made a life around it and I eventually found myself helping
+                others find their travel goals in life as well. Traveling and doing so with the love of my life, has
+                been a blessing.
+            </p>
+
+            <p>
+                Let me help you travel to your dream destinations! If you're traveling with a partner, I can help
+                organize cruise packages that are full of fun for two. Although I specialize with cruise trips to my
+                listed destinations, I can help you get to where you want in the world in some way.
             </p>
         </div>
         <div>
             <h3>Customer Comments:</h3>
 
+
             <p>
-                "Thank you Wendy for all your planning and efforts to ensure that we could enjoy our tour and bring back
-                such wonderful memories." -John Doe
+                "Julia helped my husband and I get to honeymoon destination via a an amazing cruise. Then organized
+                other travel plans accross Europe! Thank you Julia!"
+
+                <br>
+                <br>
+
+                -The Scotts
             </p>
             <p>
-                "Thank you Wendy for all your planning and efforts to ensure that we could enjoy our tour and bring back
-                such wonderful memories." -John Doe
+                "I wanted to see the great ancient wonders of the world. Julia helped me travel accross Italy and
+                Greece, by Cruise ships. It was filled with fun and afforadable packages were organized!"
+
+                <br>
+                <br>
+
+                -John
             </p>
             <p>
-                "Thank you Wendy for all your planning and efforts to ensure that we could enjoy our tour and bring back
-                such wonderful memories." -John Doe
+                "My girlfriend and I wanted to travel somewhere together. We didn't know where to go and how, or even if
+                it would work out. Julia helped us find the best way todo things and that helped us enjoy the best times
+                of our lifes."
+
+                <br>
+                <br>
+
+                -Anne
             </p>
         </div>
     </div>
