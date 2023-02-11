d9176a15528b7b0c54ad42857d493e22c3058443
ayodyln <78442693+ayodyln@users.noreply.github.com>
2023-02-11 14:38:48 -0700

    Page Layouts setq

--- CabotCruises/_top.php
+++ CabotCruises/_top.php
@@ -10,7 +10,9 @@
     <link rel="stylesheet" type="text/css" href="slick-1.8.1/slick/slick-theme.css" />
 
 
-    <title><?php echo $title; ?></title>
+    <title>
+        <?php echo $title; ?>
+    </title>
 </head>
 
 
@@ -41,10 +43,10 @@
                     </li>
 
                     <li class="parent dropdown-parent">
-                        <a href="./agents.php" class="dropdown-anchor">Agents</a>
+                        <a href="#" class="dropdown-anchor">Agents</a>
                         <ul>
-                            <li><a href="#">Sally Rodgers</a></li>
-                            <li><a href="#">Buddy Hall</a></li>
+                            <li><a href="./agent-one.php">Adam Smith</a></li>
+                            <li><a href="./agent-two.php">Julia Webster</a></li>
                         </ul>
                     </li>
                     <li><a href="./booking.php">Book</a></li>
@@ -57,7 +59,8 @@
                 <button value="search">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                         <!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
-                        <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352c79.5 0 144-64.5 144-144s-64.5-144-144-144S64 128.5 64 208s64.5 144 144 144z" />
+                        <path
+                            d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352c79.5 0 144-64.5 144-144s-64.5-144-144-144S64 128.5 64 208s64.5 144 144 144z" />
                     </svg>
                 </button>
             </form>
---
+++ CabotCruises/agent-one.php
@@ -0,0 +1,60 @@
+<?php
+$title = "Agent-Adam Smith";
+include('_top.php')
+    ?>
+
+<section id="AgentsPage">
+    <h1>
+        <?php echo $title; ?>
+    </h1>
+    <div class="agents">
+        <img src="./content/originals/AgentOne.jpg" alt="Adam Smith">
+        <div>
+            <h3>Phone:</h3>
+            <a href="tel:+1-800-555-1234">1-800-555-1234</a>
+        </div>
+        <div>
+            <h3>Working:</h3>
+            <p>Monday, Wednesday, Friday</p>
+        </div>
+        <div>
+            <h3>Destination Specialization:</h3>
+            <p>
+                Israel, Egypt, Italy, Greece, Hawaii, Guatemala, Southern California, Orlando, Las Vegas, Bahamas,
+                Caribbean cruises.
+            </p>
+        </div>
+        <div>
+            <h3>Office Hours:</h3>
+            <p>8:00am-6:00pm</p>
+        </div>
+        <div>
+            <h3>Biography:</h3>
+            <p>
+                I have loved travel for as long as I can remember. At the age of 12 I asked if I could fly by myself to
+                Hawaii and I never came back.
+                Right out of high school I started into travel school and have had the opportunity to work in many
+                aspects of the industry. In every aspect I have worked I have had the opportunity to help people travel,
+                to help fulfill people?s dreams, what could be be6er to do every day!
+            </p>
+        </div>
+        <div>
+            <h3>Customer Comments:</h3>
+
+            <p>
+                "Thank you Wendy for all your planning and efforts to ensure that we could enjoy our tour and bring back
+                such wonderful memories." -John Doe
+            </p>
+            <p>
+                "Thank you Wendy for all your planning and efforts to ensure that we could enjoy our tour and bring back
+                such wonderful memories." -John Doe
+            </p>
+            <p>
+                "Thank you Wendy for all your planning and efforts to ensure that we could enjoy our tour and bring back
+                such wonderful memories." -John Doe
+            </p>
+        </div>
+    </div>
+</section>
+
+<?php include('_bot.php') ?>
