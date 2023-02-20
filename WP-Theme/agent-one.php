75828c811765f5d3fdf1b689639d7b2f58eb3f64
ayodyln <78442693+ayodyln@users.noreply.github.com>
2023-02-20 14:19:31 -0700

    custom wp theme

---
+++ WP-Theme/agent-one.php
@@ -0,0 +1,77 @@
+<?php
+$title = "Agent-Adam Smith";
+include('_top.php')
+    ?>
+
+<section id="AgentsPage">
+    <h1>
+        <?php echo $title; ?>
+    </h1>
+    <div class="agents">
+        <img src="./content/originals/AgentOne.jpg" alt="Adam Smith">
+        <div>
+            <h3>Phone:</h3>
+            <a href="tel:+1-800-123-1234">1-800-123-1234</a>
+        </div>
+        <div>
+            <h3>Working:</h3>
+            <p>Monday, Thursday, Friday</p>
+        </div>
+        <div>
+            <h3>Destination Specialization:</h3>
+            <p>
+                Caribbean, Bahamas, Bermuda, Alaska, and Hawaii Cruises.
+            </p>
+        </div>
+        <div>
+            <h3>Office Hours:</h3>
+            <p>8:00am-4:00pm</p>
+        </div>
+        <div>
+            <h3>Biography:</h3>
+            <p>
+                I'm an advid traveller myself! Every destination I offer services for have been my favorite places to
+                travel. At a young age, I always wanted to travel the world. After college, I took that goal and made it
+                a reality, then built a career around it.
+            </p>
+            <p>
+                If you have a place you want to go, I can figure out how to get you there. My passion is travel to
+                places that inspire my heart, so I understand that want to go places. It is my goal to get you where you
+                want in this world and make the best memories ever!
+            </p>
+        </div>
+        <div>
+            <h3>Customer Comments:</h3>
+
+            <p>
+                "This was the best trip ever and the kids enjoyed every moment of it. Thank you so much for getting us
+                to such a magical place for a great price!"
+
+                <br>
+                <br>
+
+                -Williams Family
+            </p>
+            <p>
+                "I always wanted to travel the world and never knew where to start. Thanks to Adam, I was to find the
+                best way to travel and a pretty good deal."
+
+                <br>
+                <br>
+
+                -Blake
+            </p>
+            <p>
+                "I was able to see the best views of Hawaii for a super cheap price for my wife and a I. We were working
+                on a budget and always wanted to travel somewhere! Thank you!"
+
+                <br>
+                <br>
+
+                -Roger
+            </p>
+        </div>
+    </div>
+</section>
+
+<?php include('_bot.php') ?>
