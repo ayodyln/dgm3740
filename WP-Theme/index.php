a3c66a72f576fbcd280d81811af0f41fab416f48
ayodyln <78442693+ayodyln@users.noreply.github.com>
2023-04-02 12:10:32 -0600

    seo module done

--- WP-Theme/index.php
+++ WP-Theme/index.php
@@ -1,10 +1,11 @@
-<?php
+<?php get_header();
 $clientLocation = 'home';
-$title = "Cabot Cruises";
-include('header.php')
-  ?>
+$title = "Cabot Cruises"; ?>
 
+<?php while (have_posts()):
+  the_post(); ?>
+  <?php the_title('<h1>', '</h1>'); ?>
+  <?php the_content(); ?>
+<?php endwhile; ?>
 
-
-
-<?php include('footer.php') ?>
+<?php get_footer(); ?>
